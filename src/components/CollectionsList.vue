e5165fd9109e58057772de0953a959013ae1b985
Amer Tricic <amer.tricic@live.com>
2024-04-23 19:19:52 +0200

    feat: init template

---
+++ src/components/CollectionsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="collections-list p-5">
+    <!-- Info Bar-->
+    <div
+      role="button"
+      class="collections-list__info-bar bg-primary-500 br-10 pb-2"
+    >
+      <div class="collections-list__info-icon"></div>
+      <div class="collections-list__info-title fs-100">Kolekcija</div>
+      <div class="collections-list__info-count fs-100">Dokumenata</div>
+      <div class="collections-list__info-actions fs-100">Moguće radnje</div>
+    </div>
+    <!-- List -->
+    <ul class="collections-list__menu">
+      <li role="button" class="collections-list__item">
+        <div class="collections-list__icon_company"></div>
+        <div class="collections-list__title">Kompanije</div>
+        <div class="collections-list__count">30</div>
+        <button class="collections-list__btn-add">
+          <img src="/icon-add.svg" alt="" />
+          <span>Nova kompanija</span>
+        </button>
+      </li>
+
+      <li role="button" class="collections-list__item">
+        <div class="collections-list__icon_employees"></div>
+        <div class="collections-list__title">Zaposlenici</div>
+        <div class="collections-list__count">160</div>
+        <button class="collections-list__btn-add">
+          <img src="/icon-add.svg" alt="" />
+          <span>Novi zaposlenik</span>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/views/CollectionsPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-title p-5">
+    <h1 class="heading-1">Baza svih kolekcija koje aplikacija koristi</h1>
+    <ButtonInfo />
+  </div>
+  <CollectionsList />
+</template>
+
+<script>
+import ButtonInfo from "@/components/buttons/ButtonInfo.vue";
+import CollectionsList from "@/components/CollectionsList.vue";
+export default {
+  components: {
+    ButtonInfo,
+    CollectionsList,
+  },
+};
+</script>
