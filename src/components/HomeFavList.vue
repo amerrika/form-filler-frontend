c444d9e5c478ca72e87ac8101bdda324381f9e5c
Amer Tricic <amer.tricic@live.com>
2024-04-23 13:42:54 +0200

    feat: init template

---
+++ src/components/HomeFavList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fav-list">
+    <div class="fav-list__header pi-3 pb-2">
+      <h3 class="fs-500 fw-500">{{ title }}</h3>
+      <ButtonSettings />
+    </div>
+    <ul class="fav-list__menu p-3">
+      <li role="button" class="fav-list__item" v-for="item in list">{{ item.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ButtonSettings from "./buttons/ButtonSettings.vue";
+
+export default {
+  components: {
+    ButtonSettings,
+  },
+  props: {
+    title: String,
+    list: Array
+  }
+};
+</script>
