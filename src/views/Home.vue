2781962db4e0dbfe319e143f5c0258f75391739a
Amer Tricic <amer.tricic@live.com>
2024-04-23 13:47:37 +0200

    feat: init template and script

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,37 @@
 <template>
-
+  <div class="home">
+    <!-- Section -->
+    <section class="p-6">
+      <div class="section-title">
+        <h2 class="heading-2">Brza traka</h2>
+        <ButtonInfo />
+      </div>
+      <div class="even-columns">
+        <HomeFavList title="Obrasci" :list="forms" />
+        <HomeFavList title="Ugovori" :list="forms" />
+        <HomeFavList title="Dokumenti" :list="forms" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {};
+import ButtonInfo from "../components/buttons/ButtonInfo.vue";
+import HomeFavList from "../components/HomeFavList.vue";
+
+export default {
+  components: {
+    ButtonInfo,
+    HomeFavList,
+  },
+  data() {
+    return {
+      forms: [
+        { title: "Obrazac - 100" },
+        { title: "Obrazac - 200" },
+        { title: "Obrazac - 300" },
+      ],
+    };
+  },
+};
 </script>
