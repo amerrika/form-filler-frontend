963b7543c01417c12602304489d499b186343d38
Amer Tricic <amer.tricic@live.com>
2024-04-24 15:44:52 +0200

    feat: init template

---
+++ src/components/buttons/ButtonAddCompany.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-link to="/collections/companies/new" class="btn-add__company">
+    <img src="/icon-add-company.svg" alt="" />
+    <span>Novu kompaniju</span>
+  </router-link>
+  <!-- For styles look at _app-buttons.scss -->
+</template>
---
+++ src/components/buttons/ButtonAddEmployee.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-link to="/collections/employees/new" class="btn-add__employee">
+    <img src="/icon-add-employee.svg" alt="" />
+    <span>Novog zaposlenog</span>
+  </router-link>
+  <!-- For styles look at _app-buttons.scss -->
+</template>
