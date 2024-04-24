604d5a9bbc487cc5933b692641f62c5c58d247ee
Amer Tricic <amer.tricic@live.com>
2024-04-24 09:56:41 +0200

    feat: init template

---
+++ src/components/HomeFavActions.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="even-columns">
+    <div class="fav-list">
+      <div class="fav-list__header pi-3 pb-2">
+        <h3 class="fs-500 fw-500">Dodati</h3>
+      </div>
+      <ul class="fav-list__menu p-3">
+        <li role="button" class="fav-list__item">Novu kompaniju</li>
+        <li role="button" class="fav-list__item">Novog zaposlenog</li>
+      </ul>
+    </div>
+    <div class="fav-list">
+      <div class="fav-list__header pi-3 pb-2">
+        <h3 class="fs-500 fw-500">Pretražiti - Search Inputs</h3>
+      </div>
+      <ul class="fav-list__menu p-3">
+        <li role="button" class="fav-list__item">Pronađi kompaniju</li>
+        <li role="button" class="fav-list__item">Pronađi zaposlenog</li>
+      </ul>
+    </div>
+  </div>
+</template>
