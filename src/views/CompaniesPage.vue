011e5a6ce0d055fbbd58ffbc055618e7ae396196
Amer Tricic <amer.tricic@live.com>
2024-04-24 21:55:05 +0200

    feat: add CompaniesList

--- src/views/CompaniesPage.vue
+++ src/views/CompaniesPage.vue
@@ -4,15 +4,18 @@
     <ButtonInfo />
   </div>
   <CompaniesToolbar />
+  <CompaniesList />
 </template>
 <script>
 import ButtonInfo from "@/components/buttons/ButtonInfo.vue";
 import CompaniesToolbar from "@/components/CompaniesToolbar.vue";
+import CompaniesList from "@/components/CompaniesList.vue";
 
 export default {
   components: {
     ButtonInfo,
-    CompaniesToolbar
+    CompaniesToolbar,
+    CompaniesList,
   },
 };
 </script>
