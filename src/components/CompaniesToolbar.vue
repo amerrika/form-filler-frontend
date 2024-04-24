57a56242801b5ba8235d5b7a34bfac0ae405db90
Amer Tricic <amer.tricic@live.com>
2024-04-24 21:48:47 +0200

    feat: add SelectSingle

--- src/components/CompaniesToolbar.vue
+++ src/components/CompaniesToolbar.vue
@@ -2,6 +2,38 @@
   <div class="companies-toolbar mb-4">
     <div>Search input</div>
     <div>Search by</div>
-    <button>Add New</button>
+    <SelectSingle :prop-options="optionsList" />
+    <ButtonAddCompany />
   </div>
 </template>
+<script>
+import ButtonAddCompany from "./buttons/ButtonAddCompany.vue";
+import SelectSingle from "./inputs/SelectSingle.vue";
+
+export default {
+  components: {
+    SelectSingle,
+    ButtonAddCompany,
+  },
+  data() {
+    return {
+      optionsList: [
+        {
+          id: 1,
+          option: "Gradu",
+          value: "a",
+          color: "red",
+          isSelected: false,
+        },
+        {
+          id: 2,
+          option: "Samo PDV obveznike",
+          value: "b",
+          color: "yellow",
+          isSelected: false,
+        },
+      ],
+    };
+  },
+};
+</script>
