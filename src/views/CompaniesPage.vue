80acd676e490609e56b362a0bbdae659d11c1d92
Amer Tricic <amer.tricic@live.com>
2024-04-24 15:48:33 +0200

    feat: update template

--- src/views/CompaniesPage.vue
+++ src/views/CompaniesPage.vue
@@ -1,3 +1,18 @@
 <template>
-    Companies Page
+  <div class="page-title">
+    <h1 class="heading-1">Baza svih kompanija</h1>
+    <ButtonInfo />
+  </div>
+  <CompaniesToolbar />
 </template>
+<script>
+import ButtonInfo from "@/components/buttons/ButtonInfo.vue";
+import CompaniesToolbar from "@/components/CompaniesToolbar.vue";
+
+export default {
+  components: {
+    ButtonInfo,
+    CompaniesToolbar
+  },
+};
+</script>
