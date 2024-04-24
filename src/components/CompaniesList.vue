2340fc4eeb44341a90786237682648461d5744f7
Amer Tricic <amer.tricic@live.com>
2024-04-24 21:47:54 +0200

    feat: init template

---
+++ src/components/CompaniesList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="companies-list">
+    <!-- Info Bar -->
+    <div class="companies-list__info-bar p-2 bg-primary-500 br-10">
+      <div class="companies-list__data_big fs-100">ID</div>
+      <div class="companies-list__data_big fs-100">Skraćeni naziv</div>
+      <div class="companies-list__data fs-100">PDV obveznik</div>
+      <div class="companies-list__data fs-100">Općina</div>
+      <div class="companies-list__data fs-100">Telefon</div>
+    </div>
+    <!-- List -->
+    <div class="companies-list__list p-2">
+      <!-- Single Item -->
+      <div class="companies-list__item">
+        <div class="companies-list__data_big fs-200 fw-500">4200606200459</div>
+        <div class="companies-list__data_big fs-200 fw-500">
+          "Neka Firma" d.o.o. Cazin
+        </div>
+        <div class="companies-list__data fs-200 fw-500">Da</div>
+        <div class="companies-list__data fs-200 fw-500">Cazin</div>
+        <div class="companies-list__data fs-200 fw-500">+4444444</div>
+      </div>
+    </div>
+  </div>
+</template>
