8d192e677bf2a7bbc3e634eec04c45905617707e
Amer Tricic <amer.tricic@live.com>
2024-05-09 19:54:50 +0200

    feat: update template

--- src/components/CompaniesList.vue
+++ src/components/CompaniesList.vue
@@ -2,23 +2,65 @@
   <div class="companies-list">
     <!-- Info Bar -->
     <div class="companies-list__info-bar p-2 bg-primary-500 br-10">
-      <div class="companies-list__data_big fs-100">ID</div>
-      <div class="companies-list__data_big fs-100">Skraćeni naziv</div>
+      <div class="companies-list__data_big fs-100">ID broj</div>
       <div class="companies-list__data fs-100">PDV obveznik</div>
-      <div class="companies-list__data fs-100">Općina</div>
+      <div class="companies-list__data_big fs-100">Skraćeni naziv</div>
+      <div class="companies-list__data fs-100">Mjesto sjedišta</div>
       <div class="companies-list__data fs-100">Telefon</div>
+      <div class="companies-list__data fs-100">E-mail</div>
     </div>
     <!-- List -->
-    <div class="companies-list__list p-2">
+    <div class="companies-list__list mb-start-4">
       <!-- Single Item -->
       <div class="companies-list__item">
-        <div class="companies-list__data_big fs-200 fw-500">4200606200459</div>
-        <div class="companies-list__data_big fs-200 fw-500">
-          "Neka Firma" d.o.o. Cazin
-        </div>
-        <div class="companies-list__data fs-200 fw-500">Da</div>
-        <div class="companies-list__data fs-200 fw-500">Cazin</div>
-        <div class="companies-list__data fs-200 fw-500">+4444444</div>
+        <router-link to="/collections/companies/id">
+          <div class="companies-list__data_big fs-200 fw-500">
+            4200606200459
+          </div>
+          <div class="companies-list__data fs-200 fw-500">Da</div>
+          <div class="companies-list__data_big fs-200 fw-500">
+            "Neka Firma" d.o.o. Cazin
+          </div>
+          <div class="companies-list__data fs-200 fw-500">Cazin</div>
+          <div class="companies-list__data fs-200 fw-500">+4444444</div>
+          <div class="companies-list__data_big fs-200 fw-500">
+            neka-firma@email.com
+          </div>
+        </router-link>
+      </div>
+            <!-- Single Item -->
+            <div class="companies-list__item">
+        <router-link to="/collections/companies/id">
+          <div class="companies-list__data_big fs-200 fw-500">
+            4200606200459
+          </div>
+          <div class="companies-list__data fs-200 fw-500">Da</div>
+          <div class="companies-list__data_big fs-200 fw-500">
+            "Neka Firma" d.o.o. Cazin
+          </div>
+          <div class="companies-list__data fs-200 fw-500">Cazin</div>
+          <div class="companies-list__data fs-200 fw-500">+4444444</div>
+          <div class="companies-list__data_big fs-200 fw-500">
+            neka-firma@email.com
+          </div>
+        </router-link>
+      </div>
+            <!-- Single Item -->
+            <div class="companies-list__item">
+        <router-link to="/collections/companies/id">
+          <div class="companies-list__data_big fs-200 fw-500">
+            4200606200459
+          </div>
+          <div class="companies-list__data fs-200 fw-500">Da</div>
+          <div class="companies-list__data_big fs-200 fw-500">
+            "Neka Firma" d.o.o. Cazin
+          </div>
+          <div class="companies-list__data fs-200 fw-500">Cazin</div>
+          <div class="companies-list__data fs-200 fw-500">+4444444</div>
+          <div class="companies-list__data_big fs-200 fw-500">
+            neka-firma@email.com
+          </div>
+        </router-link>
       </div>
     </div>
   </div>
