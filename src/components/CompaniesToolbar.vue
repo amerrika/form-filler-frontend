12f85a673530a6ef8598a5a17f400b484d87831a
Amer Tricic <amer.tricic@live.com>
2024-05-09 19:57:58 +0200

    feat: update filtering data, accept emitted results

--- src/components/CompaniesToolbar.vue
+++ src/components/CompaniesToolbar.vue
@@ -2,38 +2,76 @@
   <div class="companies-toolbar mb-4">
     <div>Search input</div>
     <div>Search by</div>
-    <SelectSingle :prop-options="optionsList" />
+    <FilterResults
+      :filter-data="filterData"
+      @selected-options="updateSelectedFilterOptions"
+    />
     <ButtonAddCompany />
   </div>
 </template>
 <script>
 import ButtonAddCompany from "./buttons/ButtonAddCompany.vue";
-import SelectSingle from "./inputs/SelectSingle.vue";
+import FilterResults from "./inputs/FilterResults.vue";
 
 export default {
   components: {
-    SelectSingle,
+    FilterResults,
     ButtonAddCompany,
   },
   data() {
     return {
-      optionsList: [
-        {
-          id: 1,
-          option: "Gradu",
-          value: "a",
-          color: "red",
-          isSelected: false,
+      filterData: {
+        group1: {
+          id: "1",
+          title: "Mjesto sjedišta",
+          options: [
+            {
+              id: "1.1",
+              option: "Cazin",
+              value: 77220,
+              isSelected: false,
+            },
+            {
+              id: "1.2",
+              option: "Bihać",
+              value: 77000,
+              isSelected: false,
+            },
+            {
+              id: "1.3",
+              option: "Bužim",
+              value: 77000,
+              isSelected: false,
+            },
+          ],
         },
-        {
-          id: 2,
-          option: "Samo PDV obveznike",
-          value: "b",
-          color: "yellow",
-          isSelected: false,
+        group2: {
+          id: "2",
+          title: "PDV obveznik",
+          options: [
+            {
+              id: "2.1",
+              option: "DA",
+              value: "da",
+              isSelected: false,
+            },
+            {
+              id: "2.2",
+              option: "NE",
+              value: "ne",
+              isSelected: false,
+            },
+          ],
         },
-      ],
+      },
+      // options selected by user
+      selectedFilterOptions: [],
     };
+  },
+  methods: {
+    updateSelectedFilterOptions(data) {
+      this.selectedFilterOptions = data;
+    },
   },
 };
 </script>
