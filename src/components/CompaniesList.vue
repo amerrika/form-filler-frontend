6ebfb67e90fe6b292e3506b7278cef47fae20154
Amer Tricic <amer.tricic@live.com>
2024-05-11 18:46:34 +0200

    feat: update css, add props

--- src/components/CompaniesList.vue
+++ src/components/CompaniesList.vue
@@ -1,67 +1,46 @@
 <template>
   <div class="companies-list">
-    <!-- Info Bar -->
-    <div class="companies-list__info-bar p-2 bg-primary-500 br-10">
-      <div class="companies-list__data_big fs-100">ID broj</div>
-      <div class="companies-list__data fs-100">PDV obveznik</div>
-      <div class="companies-list__data_big fs-100">Skraćeni naziv</div>
-      <div class="companies-list__data fs-100">Mjesto sjedišta</div>
-      <div class="companies-list__data fs-100">Telefon</div>
-      <div class="companies-list__data fs-100">E-mail</div>
+    <!-- Head -->
+    <div class="companies-list__head p-2 bg-primary-500 br-10">
+      <div class="companies-list__data_big fs-100 fw-500">ID broj</div>
+      <div class="companies-list__data fs-100 fw-500">PDV obveznik</div>
+      <div class="companies-list__data_big fs-100 fw-500">Skraćeni naziv</div>
+      <div class="companies-list__data fs-100 fw-500">Mjesto sjedišta</div>
+      <div class="companies-list__data fs-100 fw-500">Telefon</div>
+      <div class="companies-list__data_big fs-100 fw-500">E-mail</div>
     </div>
-    <!-- List -->
-    <div class="companies-list__list mb-start-4">
-      <!-- Single Item -->
-      <div class="companies-list__item">
+    <!-- Body -->
+    <div class="companies-list__body mb-start-4">
+      <!-- Row -->
+      <div class="companies-list__row" v-for="company in companies">
         <router-link to="/collections/companies/id">
-          <div class="companies-list__data_big fs-200 fw-500">
-            4200606200459
+          <div class="companies-list__data_big fs-200">
+            {{ company.companyId }}
           </div>
-          <div class="companies-list__data fs-200 fw-500">Da</div>
-          <div class="companies-list__data_big fs-200 fw-500">
-            "Neka Firma" d.o.o. Cazin
+          <div class="companies-list__data fs-200">
+            {{ company.companyIsTaxPayer ? "Da" : "Ne" }}
           </div>
-          <div class="companies-list__data fs-200 fw-500">Cazin</div>
-          <div class="companies-list__data fs-200 fw-500">+4444444</div>
-          <div class="companies-list__data_big fs-200 fw-500">
-            neka-firma@email.com
+          <div class="companies-list__data_big fs-200">
+            {{ company.companyShortName }}
           </div>
-        </router-link>
-      </div>
-            <!-- Single Item -->
-            <div class="companies-list__item">
-        <router-link to="/collections/companies/id">
-          <div class="companies-list__data_big fs-200 fw-500">
-            4200606200459
+          <div class="companies-list__data fs-200">
+            {{ company.companyOffice.city }}
           </div>
-          <div class="companies-list__data fs-200 fw-500">Da</div>
-          <div class="companies-list__data_big fs-200 fw-500">
-            "Neka Firma" d.o.o. Cazin
+          <div class="companies-list__data fs-200">
+            {{ company.companyContact.phone }}
           </div>
-          <div class="companies-list__data fs-200 fw-500">Cazin</div>
-          <div class="companies-list__data fs-200 fw-500">+4444444</div>
-          <div class="companies-list__data_big fs-200 fw-500">
-            neka-firma@email.com
-          </div>
-        </router-link>
-      </div>
-            <!-- Single Item -->
-            <div class="companies-list__item">
-        <router-link to="/collections/companies/id">
-          <div class="companies-list__data_big fs-200 fw-500">
-            4200606200459
-          </div>
-          <div class="companies-list__data fs-200 fw-500">Da</div>
-          <div class="companies-list__data_big fs-200 fw-500">
-            "Neka Firma" d.o.o. Cazin
-          </div>
-          <div class="companies-list__data fs-200 fw-500">Cazin</div>
-          <div class="companies-list__data fs-200 fw-500">+4444444</div>
-          <div class="companies-list__data_big fs-200 fw-500">
-            neka-firma@email.com
+          <div class="companies-list__data_big fs-200">
+            {{ company.companyContact.email }}
           </div>
         </router-link>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    companies: Array,
+  },
+};
+</script>
