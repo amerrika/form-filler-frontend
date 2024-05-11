5fc564c85a2c47b12ae0c81d544a3569ba78ae2c
Amer Tricic <amer.tricic@live.com>
2024-05-11 18:45:50 +0200

    feat: refactor css, add props

--- src/components/CollectionsList.vue
+++ src/components/CollectionsList.vue
@@ -1,35 +1,40 @@
 <template>
   <div class="collections-list p-5 mb-start-5">
-    <!-- Info Bar-->
-    <div
-      role="button"
-      class="collections-list__info-bar bg-primary-500 br-10 pb-2"
-    >
-      <div class="collections-list__info-icon"></div>
-      <div class="collections-list__info-title fs-100">Kolekcija</div>
-      <div class="collections-list__info-count fs-100">Dokumenata</div>
-      <div class="collections-list__info-actions fs-100">...</div>
+    <!-- Head -->
+    <div class="collections-list__head bg-primary-500 br-10 pb-2">
+      <div class="collections-list__icon"></div>
+      <div class="collections-list__data fs-100 fw-500">Kolekcija</div>
+      <div class="collections-list__data fs-100 fw-500">Dokumenata</div>
+      <div class="collections-list__data fs-100 fw-500">...</div>
     </div>
-    <!-- List -->
-    <ul class="collections-list__menu">
-      <li role="button" class="collections-list__item">
+    <!-- Body -->
+    <div class="collections-list__body">
+      <!-- Row -->
+      <div class="collections-list__row">
         <router-link to="/collections/companies">
           <div class="collections-list__icon_company"></div>
-          <div class="collections-list__title">Kompanije</div>
-          <div class="collections-list__count">30</div>
+          <div class="collections-list__data fs-200">Kompanije</div>
+          <div class="collections-list__data fs-200">
+            {{ companiesTotalCount }}
+          </div>
         </router-link>
-      </li>
-
-      <li role="button" class="collections-list__item">
+      </div>
+      <!-- Row -->
+      <div class="collections-list__row">
         <router-link to="/collections/employees">
           <div class="collections-list__icon_employees"></div>
-          <div class="collections-list__title">Zaposlenici</div>
-          <div class="collections-list__count">160</div>
+          <div class="collections-list__data fs-200">Zaposlenici</div>
+          <div class="collections-list__data fs-200">160</div>
         </router-link>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { RouterLink } from "vue-router";
+export default {
+  props: {
+    companiesTotalCount: Number,
+  },
+};
 </script>
