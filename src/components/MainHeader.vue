6666c67ded7b2d337cd98482e8ed74dafcb8041b
Amer Tricic <amer.tricic@live.com>
2024-05-12 09:15:58 +0200

    feat: add utility classes

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="main-header" :class="minimizedStyle">
     <!-- Header Top-->
-    <div class="main-header__top">
+    <div class="main-header__top p-3">
       <span class="main-header__logo" v-if="!minimized">Form Filler</span>
       <button
         :class="[
