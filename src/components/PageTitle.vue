b93d48d8b8ed1ad1d632e1364c5ca118c79321b1
Amer Tricic <amer.tricic@live.com>
2024-05-13 19:22:11 +0200

    feat: init commit

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-title">
+    <h1 class="heading-1">{{ title }}</h1>
+    <ButtonInfo />
+  </div>
+</template>
+<script>
+import ButtonInfo from "./buttons/ButtonInfo.vue";
+export default {
+  props: { title: String },
+  components: {
+    ButtonInfo,
+  },
+};
+</script>
---
+++ src/views/NewCompanyPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <PageTitle title="Dodaj novu kompaniju" />
+</template>
+<script>
+import PageTitle from "@/components/PageTitle.vue";
+
+export default {
+  components: {
+    PageTitle,
+  },
+};
+</script>
