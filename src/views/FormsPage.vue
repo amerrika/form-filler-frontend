aa21eaee60f1a0e71ced91367a26828de573d22a
Amer Tricic <amer.tricic@live.com>
2024-05-16 12:35:39 +0200

    feat: rename, update template

---
+++ src/views/FormsPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <PageTitle title="Obrasci" icon-src="/icon-form.png" />
+</template>
+
+<script>
+import PageTitle from '@/components/PageTitle.vue';
+export default {
+    components: {
+        PageTitle,
+    }
+};
+</script>
