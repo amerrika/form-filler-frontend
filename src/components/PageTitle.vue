638d404780208e20054e9842e95da88ff20246d8
Amer Tricic <amer.tricic@live.com>
2024-05-16 12:24:38 +0200

    feat: update template and props

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="page-title">
-    <h1 class="heading-1">{{ title }}</h1>
+    <img :src="iconSrc" class="page-title__icon" alt="" />
+    <h1 class="page-title__heading heading-1">{{ title }}</h1>
     <ButtonInfo />
   </div>
 </template>
 <script>
 import ButtonInfo from "./buttons/ButtonInfo.vue";
 export default {
-  props: { title: String },
+  props: {
+    title: {
+      type: String,
+      default: "Page Title is Missing",
+    },
+    iconSrc: {
+      type: String,
+    },
+  },
   components: {
     ButtonInfo,
   },
