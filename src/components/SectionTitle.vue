c968e36a37f1c17074e6c57958546fba68aa82fc
Amer Tricic <amer.tricic@live.com>
2024-05-16 19:22:28 +0200

    feat: first init

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="section-title">
+    <img :src="iconSrc" class="section-title__icon" alt="" />
+    <h2 class="section-title__heading heading-2">{{ title }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "A Title Is Missing",
+    },
+    iconSrc: {
+      type: String,
+    },
+  },
+};
+</script>
