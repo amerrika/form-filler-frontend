ce90e53cf7b9e9024fe78059aae41f8cc0e88fa7
Amer Tricic <amer.tricic@live.com>
2024-05-17 19:40:24 +0200

    feat: update template

--- src/views/NewCompanyPage.vue
+++ src/views/NewCompanyPage.vue
@@ -1,20 +1,35 @@
 <template>
-  <PageTitle title="Dodaj novu kompaniju u bazu" icon-src="/icon-add-company.svg" />
-  <SelectSingle
-    btn-text="Izaberite opciju"
-    :select-options="selectOptions"
-    @selected-option="updateSelectedOption"
-  />
-  <InputText label="Puna firma" input-id="test-id" />
+  <div class="new-company">
+    <PageTitle
+      title="Dodavanje nove kompanije u bazu"
+      icon-src="/icon-add-company.svg"
+    />
+    <section class="new-company__section p-5 br-10 mb-start-5">
+      <div class="even-columns">
+        <div>
+          <SectionTitle title="Podaci o firmi" />
+          <InputText label="Puna firma" input-id="puna-firma" />
+          <InputText label="Skraćena firma" input-id="skracena-firma" />
+        </div>
+        <div>
+          <SectionTitle title="Identifikacioni podaci" />
+          <InputText label="Puna firma" input-id="puna-firma" />
+          <InputText label="Skraćena firma" input-id="skracena-firma" />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
 import PageTitle from "@/components/PageTitle.vue";
 import SelectSingle from "@/components/inputs/SelectSingle.vue";
 import InputText from "@/components/inputs/InputText.vue";
+import SectionTitle from "@/components/SectionTitle.vue";
 
 export default {
   components: {
     PageTitle,
+    SectionTitle,
     SelectSingle,
     InputText,
   },
