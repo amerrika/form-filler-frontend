4bf8ad595af59d382d56c9317c8f14d2fc9cffda
Amer Tricic <amer.tricic@live.com>
2024-05-23 10:30:13 +0200

    feat: update template, update components

--- src/views/NewCompanyPage.vue
+++ src/views/NewCompanyPage.vue
@@ -4,59 +4,19 @@
       title="Dodavanje nove kompanije u bazu"
       icon-src="/icon-add-company.svg"
     />
-    <section class="new-company__section p-5 br-10 mb-start-5">
-      <div class="even-columns">
-        <div>
-          <SectionTitle title="Podaci o firmi" />
-          <InputText label="Puna firma" input-id="puna-firma" />
-          <InputText label="Skraćena firma" input-id="skracena-firma" />
-        </div>
-        <div>
-          <SectionTitle title="Identifikacioni podaci" />
-          <InputText label="Puna firma" input-id="puna-firma" />
-          <InputText label="Skraćena firma" input-id="skracena-firma" />
-        </div>
-      </div>
-    </section>
+    <NewCompanyForm />
   </div>
 </template>
 <script>
 import PageTitle from "@/components/PageTitle.vue";
-import SelectSingle from "@/components/inputs/SelectSingle.vue";
-import InputText from "@/components/inputs/InputText.vue";
 import SectionTitle from "@/components/SectionTitle.vue";
+import NewCompanyForm from "@/components/NewCompanyForm.vue";
 
 export default {
   components: {
     PageTitle,
     SectionTitle,
-    SelectSingle,
-    InputText,
-  },
-  data() {
-    return {
-      selectOptions: [
-        {
-          id: "1",
-          name: "Bihać",
-          value: 77200,
-          isSelected: false,
-        },
-        {
-          id: "2",
-          name: "Cazin",
-          value: 77220,
-          isSelected: false,
-        },
-      ],
-      selectedOption: null,
-    };
-  },
-  methods: {
-    updateSelectedOption(data) {
-      console.log(data);
-      this.selectedOption = data;
-    },
+    NewCompanyForm,
   },
 };
 // All data provided by the user will be saved
@@ -64,4 +24,5 @@ export default {
 // id number
 // city
 // isTaxpayer...
+// from here to Store/DB
 </script>
