6fe4458a2cf138462b5a96f61010d2b4746f727b
Amer Tricic <amer.tricic@hotmail.com>
2024-06-10 17:27:43 +0200

    feat: use companyStore, fetch all companies

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,26 @@
 </template>
 
 <script>
+// Vue Components
 import MainHeader from "./components/MainHeader.vue";
+// Scss
 import "../src/scss/main.scss";
+// Pinia Store (used without setup function)
+import { useCompanyStore } from "@/stores/companyStore";
+import { mapStores, setMapStoreSuffix } from "pinia";
+setMapStoreSuffix("");
 
 export default {
   components: {
     MainHeader,
+  },
+  computed: {
+    // using Pinia without setup function
+    ...mapStores(useCompanyStore),
+  },
+  created() {
+    // fetch companies
+    this.companyStore.getCompanies();
   },
 };
 </script>
