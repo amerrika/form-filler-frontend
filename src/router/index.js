74b3d336188508f523608a720bf4b1fbfa5da18d
Amer Tricic <amer.tricic@hotmail.com>
2024-06-15 10:29:51 +0200

    feat: update routes because views folder renamed to pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import FormsPage from "@/views/FormsPage.vue";
-import CollectionsPage from "@/views/CollectionsPage.vue";
-import CompaniesPage from "@/views/CompaniesPage.vue";
-import EmployeesPage from "@/views/EmployeesPage.vue";
-import NewCompanyPage from "@/views/NewCompanyPage.vue";
+import Home from "../pages/Home.vue";
+import FormsPage from "@/pages/FormsPage.vue";
+import CollectionsPage from "@/pages/CollectionsPage.vue";
+import CompaniesPage from "@/pages/CompaniesPage.vue";
+import EmployeesPage from "@/pages/EmployeesPage.vue";
+import NewCompanyPage from "@/pages/NewCompanyPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
