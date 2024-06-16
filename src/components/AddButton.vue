5ca7d34dd1d7a36ece0c268ffbc1ab553945315c
Amer Tricic <amer.tricic@hotmail.com>
2024-06-16 12:46:41 +0200

    feat: move files back to components folder

---
+++ src/components/AddButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link to={{ linkTo }} class="btn-add__employee">
+    <img src={{ iconSrc }} alt="" />
+    <span>{{ btnText }}</span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    btnText: {
+      type: String,
+      required: true,
+      default: "Button Name",
+    },
+    iconSrc: {
+      type: String,
+      required: true,
+      default: "/icons/icon-is-missing.svg",
+    },
+    linkTo: {
+      type: String,
+      required: true,
+      default: "",
+    },
+  },
+};
+</script>
--- src/components/AddButtonEmployee.vue
+++ src/components/AddButtonEmployee.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/collections/employees/new" class="btn-add__employee">
-    <img src="/icon-add-employee.svg" alt="" />
+    <img src="/icons/icon-add-employee.svg" alt="" />
     <span>Novog zaposlenog</span>
   </router-link>
 </template>
