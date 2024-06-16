a0cd169592e99a2c1eb2744664b090a7318a38ba
Amer Tricic <amer.tricic@hotmail.com>
2024-06-16 16:43:12 +0200

    feat: correct icon path of SectionTitle

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@
     <section class="home__section p-5 br-10 mb-start-5">
       <SectionTitle
         title="Često korišteni dokumenti"
-        icon-src="/icon-favorite.svg"
+        icon-src="/icons/icon-favorite.svg"
       />
       <div class="even-columns">
         <HomeFavList title="Obrasci" :list="forms" />
@@ -17,7 +17,7 @@
     <section class="home__section p-5 br-10 mb-start-5">
       <SectionTitle
         title="Često korištene radnje"
-        icon-src="/icon-favorite.svg"
+        icon-src="/icons/icon-favorite.svg"
       />
       <HomeFavActions />
     </section>
