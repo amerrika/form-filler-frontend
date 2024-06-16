c8a4a8685af1ef32e241b4d5267f6a0e0e118c22
Amer Tricic <amer.tricic@hotmail.com>
2024-06-16 12:55:51 +0200

    feat: update icon path

--- src/components/AddButtonCompany.vue
+++ src/components/AddButtonCompany.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/collections/companies/new" class="btn-add__company">
-    <img src="/icon-add-company.svg" alt="" />
+    <img src="/icons/icon-add-company.svg" alt="" />
     <span>Novu kompaniju</span>
   </router-link>
 </template>
