bcdf04ea1fee9d0b4f99a18f1fffd39bd900ff85
Amer Tricic <amer.tricic@hotmail.com>
2024-06-17 14:12:58 +0200

    feat: remove fav list components, better solution is necessary

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <PageTitle title="Početna stranica aplikacije" icon-src="/icons/icon-home.png" />
+    <PageTitle
+      title="Početna stranica aplikacije"
+      icon-src="/icons/icon-home.png"
+    />
     <!-- Section -->
     <section class="home__section p-5 br-10 mb-start-5">
       <SectionTitle
@@ -8,9 +11,6 @@
         icon-src="/icons/icon-favorite.svg"
       />
       <div class="even-columns">
-        <HomeFavList title="Obrasci" :list="forms" />
-        <HomeFavList title="Ugovori" :list="forms" />
-        <HomeFavList title="Dokumenti" :list="forms" />
       </div>
     </section>
     <!-- Section -->
@@ -19,32 +19,45 @@
         title="Često korištene radnje"
         icon-src="/icons/icon-favorite.svg"
       />
-      <HomeFavActions />
+      <!-- List Box -->
+      <div class="list-box-fav br-10">
+        <!-- Header -->
+        <div class="list-box-fav__header pi-4 pb-2 bg-primary-500">
+          <h3 class="fs-500 fw-500 txt-neutral-600">Dodaj u bazu</h3>
+          <button class="btn-icon_settings"></button>
+        </div>
+        <!-- Main -->
+        <ul class="list-box-fav__menu p-3 bg-neutral-100">
+          <li role="button" class="list-box-fav__item fs-300 p-2">
+            <AddButton
+              icon-src="/icons/icon-add-employee.svg"
+              btn-text="Novog zaposlenog"
+              link-to="/collections/employees/new"
+            />
+          </li>
+          <li role="button" class="list-box-fav__item fs-300 p-2">
+            <AddButton
+              icon-src="/icons/icon-add-company.svg"
+              btn-text="Novu kompaniju"
+              link-to="/collections/companies/new"
+            />
+          </li>
+        </ul>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
-import HomeFavList from "../components/HomeFavList.vue";
-import HomeFavActions from "../components/HomeFavActions.vue";
 import PageTitle from "@/components/PageTitle.vue";
 import SectionTitle from "@/components/SectionTitle.vue";
+import AddButton from "@/components/AddButton.vue";
 
 export default {
   components: {
+    AddButton,
     PageTitle,
     SectionTitle,
-    HomeFavList,
-    HomeFavActions,
-  },
-  data() {
-    return {
-      forms: [
-        { title: "Obrazac - 100" },
-        { title: "Obrazac - 200" },
-        { title: "Obrazac - 300" },
-      ],
-    };
   },
 };
 </script>
