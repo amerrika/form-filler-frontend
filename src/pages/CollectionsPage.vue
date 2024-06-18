935f37ca93a99f68f983fc4f6df703a9700c61c6
Amer Tricic <amer.tricic@hotmail.com>
2024-06-18 11:23:53 +0200

    feat: update component that has been renamed

--- src/pages/CollectionsPage.vue
+++ src/pages/CollectionsPage.vue
@@ -1,12 +1,12 @@
 <template>
   <PageTitle title="Baza svih kolekcija" icon-src="/icons/icon-database.png" />
-  <CollectionsList :companies-total-count="companyStore.totalCount" />
+  <CollectionsPageList :companies-total-count="companyStore.totalCount" />
 </template>
 
 <script>
 // Vue Components
 import PageTitle from "@/components/PageTitle.vue";
-import CollectionsList from "@/components/CollectionsList.vue";
+import CollectionsPageList from "@/components/CollectionsPageList.vue";
 // Pinia Store (used without setup function)
 import { useCompanyStore } from "@/stores/companyStore";
 import { mapStores, setMapStoreSuffix } from "pinia";
@@ -15,7 +15,7 @@ setMapStoreSuffix("");
 export default {
   components: {
     PageTitle,
-    CollectionsList,
+    CollectionsPageList,
   },
   computed: {
     // using Pinia without setup function
