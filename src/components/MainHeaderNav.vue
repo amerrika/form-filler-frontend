dd42de0c4997bb58419e760d3373d6874dbc7892
Amer Tricic <amer.tricic@hotmail.com>
2024-06-18 13:11:30 +0200

    feat: correct icons, links to and link names

--- src/components/MainHeaderNav.vue
+++ src/components/MainHeaderNav.vue
@@ -7,18 +7,18 @@
           activeClass="bg-primary-500"
           class="main-nav__link txt-neutral-800 fs-300 fw-500"
         >
-          <img class="main-nav__icon" src="/icons/icon-home.png" alt="" />
+          <img class="main-nav__icon" src="/icons/icon-home.svg" alt="" />
           <span>Početna</span>
         </router-link>
       </li>
       <li class="main-nav__item">
         <router-link
-          to="/forms"
+          to="/documents"
           activeClass="bg-primary-500"
           class="main-nav__link txt-neutral-800 fs-300 fw-500"
         >
-          <img class="main-nav__icon" src="/icons/icon-form.png" alt="" />
-          <span>Obrasci</span>
+          <img class="main-nav__icon" src="/icons/icon-document.svg" alt="" />
+          <span>Dokumenti</span>
         </router-link>
       </li>
       <li class="main-nav__item">
@@ -27,8 +27,8 @@
           activeClass="bg-primary-500"
           class="main-nav__link txt-neutral-800 fs-300 fw-500"
         >
-          <img class="main-nav__icon" src="/icons/icon-database.png" alt="" />
-          <span>Kolekcije</span>
+          <img class="main-nav__icon" src="/icons/icon-database.svg" alt="" />
+          <span>Baza podataka</span>
         </router-link>
       </li>
     </ul>
