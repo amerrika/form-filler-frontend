0d0ffe36d320b063002a16c7975c141fe5ede50e
Amer Tricic <amer.tricic@hotmail.com>
2024-06-22 21:43:15 +0200

    feat: initial commit

---
+++ src/components/DocumentPageViewer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="document-viewer">
+    <div class="document-viewer__outer">
+      <div class="document-viewer__inner">
+        <Letter1 />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Import Vue components
+import Letter1 from "./print-documents/Letter1.vue";
+
+export default {
+  components: {
+    Letter1,
+  },
+};
+</script>
