4504ea3bf048a61944bbe527acb0f757204960f6
Amer Tricic <amer.tricic@hotmail.com>
2024-06-22 21:47:06 +0200

    feat: import components and add to the template

--- src/pages/DocumentPage.vue
+++ src/pages/DocumentPage.vue
@@ -1,3 +1,27 @@
 <template>
-    Document Page
+  <div class="document-page">
+    <PageTitle
+      title="Uredite odabrani dokument"
+      icon-src="/icons/icon-edit-document.svg"
+    />
+    <div class="flex-end g-4">
+      <DocumentPageViewer />
+      <DocumentPageSidebar />
+    </div>
+  </div>
 </template>
+
+<script>
+// Import Vue components
+import DocumentPageSidebar from "@/components/DocumentPageSidebar.vue";
+import DocumentPageViewer from "@/components/DocumentPageViewer.vue";
+import PageTitle from "@/components/PageTitle.vue";
+
+export default {
+  components: {
+    DocumentPageViewer,
+    DocumentPageSidebar,
+    PageTitle,
+  },
+};
+</script>
