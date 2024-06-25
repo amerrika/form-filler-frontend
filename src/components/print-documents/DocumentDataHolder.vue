2b887ba2cccc0ca50a1d031998d5f93929b30667
Amer Tricic <amer.tricic@hotmail.com>
2024-06-25 20:19:46 +0200

    feat: initial commit

---
+++ src/components/print-documents/DocumentDataHolder.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class="data-holder">{{ data }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: String,
+      required: false,
+      default: "Potreban podatak"
+    }
+  }
+};
+</script>
